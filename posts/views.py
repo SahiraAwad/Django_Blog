9eed987e26e2fbd7ab8982ad366604ba6d34054c
SahiraAwad <saherahussein1988@gmail.com>
2023-11-30 23:28:27 +0300

    add views functions and url for templates pages

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def post_list(request):
+
+    data = Post.objects.all()
+    context={
+        'data': data
+        }
+    return render(request,'posts/post_list.html',context)
+    
+
+def post_detail(request,post_id):
+    data = Post.objects.get(id=post_id)
+
+    context={
+        'post': data
+        }
+    return render(request,'posts/post_detail.html',context)
+
+    
+
