7dbc14485f93e9a22366838ec306c416fc7d6b74
SahiraAwad <saherahussein1988@gmail.com>
2023-11-30 20:19:04 +0300

    add tags to models

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from taggit.managers import TaggableManager
 
 # Create your models here.
 class Post(models.Model):
@@ -7,6 +8,8 @@ class Post(models.Model):
     content = models.TextField(max_length= 1000)
     draft = models.BooleanField(default=True)
     publish_date = models.DateTimeField(default=timezone.now)
+    
+    tags = TaggableManager()
 
     def __str__(self):
          return self.title
