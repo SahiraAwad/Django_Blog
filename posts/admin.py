5a4d61cb35593b50a5f9cc8d5ca9bd9a6d725e90
SahiraAwad <saherahussein1988@gmail.com>
2023-11-30 22:42:25 +0300

    add comments to models

--- posts/admin.py
+++ posts/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post , Category , Comment
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display =['title','draft']
-    list_filter=['draft','tags']
+    list_display =['title','draft','category']
+    list_filter=['draft','tags','category']
     search_fields =['title']
 
 
 admin.site.register(Post,ProductAdmin)
+admin.site.register(Category)
+admin.site.register(Comment)
 
 # Register your models here.
--- posts/models.py
+++ posts/models.py
@@ -25,5 +25,16 @@ class Post(models.Model):
          return self.title
 
 
+class Comment(models.Model):
+     post =models.ForeignKey(Post,related_name= 'comment_post',on_delete=models.CASCADE)      
+     user = models.CharField(max_length=50)  
+     comment= models.TextField(max_length=300)
+     created_at = models.DateTimeField(default=timezone.now)
+
+def __str__(self):
+     return str(self.post)    
+
+
+
 
 
