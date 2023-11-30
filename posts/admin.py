4919bd1ba23b047742087d35c462f13d55c4a372
SahiraAwad <saherahussein1988@gmail.com>
2023-11-30 23:43:17 +0300

    Apply summernote to Textfield

--- posts/admin.py
+++ posts/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from .models import Post , Category , Comment
+from django_summernote.admin import SummernoteModelAdmin
 
 
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(SummernoteModelAdmin):
     list_display =['title','draft','category']
     list_filter=['draft','tags','category']
     search_fields =['title']
+    summernote_fields = ['content']
 
 
 admin.site.register(Post,ProductAdmin)
